342d1b5a36d96462233e168245d1483960c42475
guna <gunanarahari@gmail.com>
2026-02-18 13:52:27 +0530

    commit

--- config/Mailer.js
+++ config/Mailer.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
-  family: 4, 
+  family: 4, // force IPv4 (important for Render)
   tls: {
     rejectUnauthorized: false,
   },
