a14a5cb854cdfa8ff45af5a97ab3bb2be2a0f8a6
guna <gunanarahari@gmail.com>
2026-02-18 13:02:35 +0530

    fifth

--- config/Mailer.js
+++ config/Mailer.js
@@ -2,11 +2,11 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,          
-  secure: false,    
+  port: 587,           
+  secure: false,        
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    pass: process.env.EMAIL_PASSWORD, 
   },
   tls: {
     rejectUnauthorized: false,
