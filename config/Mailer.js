24c10b0002602cb4c217fae78d742a1a86e4f3ef
guna <gunanarahari@gmail.com>
2026-02-20 11:10:27 +0530

    ten

--- config/Mailer.js
+++ config/Mailer.js
@@ -16,6 +16,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true only for 465
+  family: 4, // 👈 FORCE IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
